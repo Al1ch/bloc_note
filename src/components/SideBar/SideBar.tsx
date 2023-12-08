147f7170b6b573c424a28a1aec741bc58bb08d72
Alain Chea <alain.chea@efrei.net>
2023-12-08 12:57:40 +0100

    feat(project): init

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SideBar.module.scss";
+
+const SideBar = () => {
+  return <div className={styles.container}>a</div>;
+};
+
+export default SideBar;
