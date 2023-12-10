0d18493af37691c44be19b19dd9096bc0f9ec0ae
Alain Chea <alain.chea@efrei.net>
2023-12-10 13:20:38 +0100

    feat(components): sidebar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "./SideBar.module.scss";
+import NoteCard from "../NoteCard/NoteCard";
 
 const SideBar = () => {
-  return <div className={styles.container}>a</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Bloc notes </h1>
+      <div className={styles.noteList}>
+        <NoteCard title="Note 1" date="01/01/2021" />
+        <NoteCard title="Note 1" date="01/01/2021" />
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
