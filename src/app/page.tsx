8df49101016fa1eeb6b1a5c31633b2db12d696eb
Alain Chea <alain.chea@efrei.net>
2023-12-10 13:21:00 +0100

    feat(page): add styles and body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
 import SideBar from "@/components/SideBar/SideBar";
 import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
       <SideBar />
-      <div>azzaezae</div>
+      <div className={styles.body}>
+        <p className={styles.sentence}>
+          Click a note on the left to view something !
+        </p>
+      </div>
     </main>
   );
 }
