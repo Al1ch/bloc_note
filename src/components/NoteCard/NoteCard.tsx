1eadc1adf694073be315f2831eb4ffc49457464f
Alain Chea <alain.chea@efrei.net>
2023-12-11 09:38:38 +0100

    feat(components):  Note Card

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import styles from "./NoteCard.module.scss";
+import Link from "next/link";
 
 type Props = {
   title: string;
   date: string;
+  key: number;
+  id: number;
 };
 
-const NoteCard = ({ title, date }: Props) => {
+const NoteCard = ({ title, date, key, id }: Props) => {
+  console.log("ID ", id);
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.date}>{date}</p>
-    </div>
+    <Link href={`/notes/${id}`} className={styles.link}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.date}>{date}</p>
+      </div>
+    </Link>
   );
 };
 
