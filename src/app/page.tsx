0a9248605fbcf927d0b6fe299e5b438c995d2132
Alain Chea <alain.chea@efrei.net>
2023-12-12 17:00:06 +0100

    refactor(pages): added layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,9 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <SideBar />
-      <div className={styles.body}>
-        <p className={styles.sentence}>
-          Click a note on the left to view something !
-        </p>
-      </div>
+      <p className={styles.sentence}>
+        Click a note on the left to view something !
+      </p>
     </main>
   );
 }
