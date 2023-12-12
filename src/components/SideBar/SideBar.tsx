0a8961987e7e5a6a7ae4ff91a832a82bb39fb9ca
Alain Chea <alain.chea@efrei.net>
2023-12-12 16:47:53 +0100

    feat(component): sidebar add date format

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import styles from "./SideBar.module.scss";
 import NoteCard from "../NoteCard/NoteCard";
+import { getNotes } from "@/lib/notes";
 
-const SideBar = () => {
+const SideBar = async () => {
+  const notes = await getNotes();
+
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Bloc notes </h1>
       <div className={styles.noteList}>
-        <NoteCard title="Note 1" date="01/01/2021" />
-        <NoteCard title="Note 1" date="01/01/2021" />
+        {notes?.map((note) => (
+          <NoteCard
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            date={note.createdAt.toLocaleDateString("en-GB", options)}
+          />
+        ))}
       </div>
     </div>
   );
