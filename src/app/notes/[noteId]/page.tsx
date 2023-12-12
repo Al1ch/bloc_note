5fc26b4081a68ecdfb05468efdb7cc7c90caa1f7
Alain Chea <alain.chea@efrei.net>
2023-12-12 16:52:44 +0100

    feat(page): add dynamic routing

---
+++ src/app/notes/[noteId]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SideBar from "@/components/SideBar/SideBar";
+
+const NotesPage = ({ params }: { params: { userName: string } }) => {
+  return <div>15</div>;
+};
+
+export default NotesPage;
