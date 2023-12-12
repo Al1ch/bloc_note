cf40fdf78fb55d9c15b10355f87b7892918fae88
Alain Chea <alain.chea@efrei.net>
2023-12-12 22:24:09 +0100

    feat(page): NotesPage

--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "@/components/SideBar/SideBar";
+import styles from "./Note.module.scss";
+import prisma from "@/lib/prisma";
+import Button from "@/components/Button/Button";
 
-const NotesPage = ({ params }: { params: { userName: string } }) => {
-  return <div>15</div>;
-};
+export default async function NotesPage({
+  params,
+}: {
+  readonly params: { readonly userName: string };
+}) {
+  const note = await prisma.note.findUnique({
+    where: {
+      id: Number(params.userName),
+    },
+  });
 
-export default NotesPage;
+  return (
+    <div className={styles.container}>
+      <div className={styles.notesContainer}>
+        <div className={styles.header}>
+          <div className={styles.titleSection}>
+            <h1> Title</h1>
+            <p className={styles.date}> Date</p>
+          </div>
+          <Button size="md" backgroundColor="blue">
+            Edit
+          </Button>
+        </div>
+        <div className={styles.content}>
+          <p className={styles.text}> Content sEE YOUU SMILEEE smileee</p>
+        </div>
+      </div>
+    </div>
+  );
+}
