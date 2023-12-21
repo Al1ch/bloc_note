4769d8d708c0f7f9c2ee4f965496fa95600f0a8c
Alain Chea <alain.chea@efrei.net>
2023-12-21 15:04:06 +0100

    feat(components): make generic Note form

---
+++ src/components/NoteForm/NoteForm.tsx
@@ -0,0 +1,96 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import styles from "./NoteForm.module.scss";
+import SaveEditButton from "@/components/Buttons/Button/SaveEditButton/SaveEditButton";
+import Button from "../Buttons/Button/Button";
+import NoteWithMarkdown from "../NoteWithMarkdown/NoteWithMarkdown";
+import AddNoteButton from "../Buttons/Button/AddNoteButton.tsx/AddNoteButton";
+import CreateNoteButton from "../CreateNote/CreateNoteButton";
+
+type Props = {
+  title?: string;
+  content?: string;
+  createdAt?: Date;
+  noteId?: string;
+  type: "edit" | "create";
+};
+
+const NoteForm = ({ title, content, createdAt, noteId, type }: Props) => {
+  const [titleState, setTitle] = useState(title);
+  const [contentState, setContent] = useState(content);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  // useEffect(() => {
+  //   adjustTextAreaHeight();
+  // }, [textAreaRef]);
+
+  // const adjustTextAreaHeight = () => {
+  //   const textArea = textAreaRef.current;
+  //   if (textArea) {
+  //     textArea.style.height = "auto"; // Réinitialise la hauteur à la valeur par défaut pour recalculer la hauteur réelle.
+  //     textArea.style.height = textArea.scrollHeight + "px"; // Ajuste la hauteur en fonction du contenu réel.
+  //   }
+  // };
+  const phrase = titleState && titleState + "\n" + contentState;
+
+  return (
+    <div className={styles.editPageContainer}>
+      <form
+        ref={formRef}
+        className={styles.container}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Title"
+          value={titleState}
+          className={styles.input}
+          onChange={(e) => setTitle(e.target.value)}
+          name="titleContent"
+        />
+        <textarea
+          ref={textAreaRef}
+          placeholder="Content"
+          value={contentState}
+          className={styles.contentContainer}
+          onChange={(e) => setContent(e.target.value)}
+          name="content"
+        />
+      </form>
+      <div className={styles.previewContainer}>
+        {type === "edit" ? (
+          <SaveEditButton
+            title={titleState}
+            content={contentState}
+            noteId={noteId}
+          />
+        ) : (
+          <CreateNoteButton
+            title={titleState as string}
+            content={contentState as string}
+          />
+        )}
+
+        <div className={styles.notePreview}>
+          <Button
+            backgroundColor="lightBlue"
+            size="md"
+            type="submit"
+            borderRadius="circle"
+          >
+            <span className={styles.buttonText}>PREVIEW</span>
+          </Button>
+          <div className={styles.notePreviewContent}>
+            <NoteWithMarkdown text={phrase as string} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteForm;
