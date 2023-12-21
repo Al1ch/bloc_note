3390ba7c303732c5108b33310b36cdc173fc0aed
Alain Chea <alain.chea@efrei.net>
2023-12-21 15:00:08 +0100

    feat(_action): server action fction

--- src/app/_action.ts
+++ src/app/_action.ts
@@ -1,6 +1,22 @@
 'use server'
-import { updateNoteTable } from "@/lib/notes"
+import { updateNoteTable , addNote, getNotes} from "@/lib/notes"
+import { revalidatePath } from "next/cache";
 
 export const updateNote = async(noteId? : string, title? : string, content? : string) =>{
     await updateNoteTable(noteId, title, content);
+    revalidatePath(`/notes/${noteId}`);
+
+}
+
+
+export const createNote = async(title: string, content : string) => {
+    const note = await addNote (title, content);
+    revalidatePath(`/notes/${note?.id}`);
+    return note?.id;
+}
+
+export const getAllNotes = async() => {
+    const notes = await getNotes();
+    revalidatePath('/notes/2');
+    return notes;
 }
--- src/lib/notes.ts
+++ src/lib/notes.ts
@@ -18,7 +18,11 @@ export const getNote = async (noteId : string) => {
 
 export const getNotes = async () => {
     try{
-        const notes = await prisma.note.findMany();
+        const notes = await prisma.note.findMany({
+            orderBy:{
+                id: 'desc',
+            }
+        });
         return notes;
     }
     catch(err){
@@ -37,6 +41,23 @@ export const updateNoteTable = async (noteId?: string, title? : string, content?
                 content: content,
             }
         });
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+export const addNote = async(title: string, content : string) => {
+    try{
+        const note = await prisma.note.create({
+            data: {
+                title: title,
+                content: content,
+            }
+        });
+
+        return note;
+        
     }
     catch(err){
         console.log(err);
